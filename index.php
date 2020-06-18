0ecb52bf16af9b0480989137393aea41ae6745b4
anupkafle <43605334+anupkafle@users.noreply.github.com>
2020-06-18 19:16:23 +0545

    Add files via upload

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
         <!-- css -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
         <!-- script -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
     <!-- navbar start -->
-
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
@@ -66,29 +67,90 @@
     </div>
 </nav>
 <!-- navbar end -->
-<footer class = "footer text-dark">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-12 p-3">
-            <h6 class="text-uppercase font-weight-bold ">Additional Information</h6>
-            <p>
-              Feel free to contact us for any type of information.
-            </p>
-          </div>
-          <div class="col-md-4 col-sm-12 p-3">
-            <h6 class="text-uppercase font-weight-bold">Contact</h6>
-              <br />inf0@infonity.com
-              <br />+977 4242121</p>
-          </div>
+<footer class="mainfooter" role="contentinfo">
+  <div class="footer-middle">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 col-sm-6">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>Heading 1</h4>
+          <ul class="list-unstyled">
+            <li><a href="#"></a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Sport</a></li>
+            <li><a href="#">Tech</a></li>
+            <li><a href="#">News and Updates</a></li>
+            <li><a href="#">Others</a></li>
+          </ul>
         </div>
       </div>
-      <div class="footer-copyright text-center pb-3">&copy; 2019 Copyright:infonity.com</div>
-    </footer>
+      <div class="col-md-3 col-sm-6">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>Heading 2</h4>
+          <ul class="list-unstyled">
+            <li><a href="#">Hot News</a></li>
+            <li><a href="#">Recently added</a></li>
+            <li><a href="#">Trending</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>Heading 3</h4>
+          <ul class="list-unstyled">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Disclamer</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li>
+              <a href="#"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    	<div class="col-md-3">
+    		<h4>Follow Us</h4>
+            <ul class="social-network social-circle">
+             <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+             <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+             <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+            </ul>				
+        </div>
+
+        <!-- Newsletter -->
+        
+        <div class="container-fluid justify-content-center" >
+        <div class="row py-5">
+        <div class="col">
+            <div class="card border-0">
+                <div class="card-body text-center ">
+                    <h2><b>Subscribe to our Newsletter !</b></h2>
+                    <p class="pl-0 ml-0 mb-5">And never miss any update from us.</p>
+                    <div class="row text-center justify-content-center">
+                        <div class="col-auto">
+                            <div class="input-group-lg input-group mb-3 "><input type="text" class="form-control" placeholder="Enter your e-mail address" aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <div class="input-group-append"><button class="btn btn-success" type="button" id="button-addon2"> <b>Submit</b></button></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+	<div class="row">
+		<div class="col-md-12 copy">
+			<p class="text-center">&copy; Copyright 2020 - Info-Nity.  All rights reserved.</p>
+		</div>
+	</div>
 
 
+  </div>
+  </div>
+</footer>
 </body>
 </html>
-
-
-
-    
