ee2db8a36067f9d83cc8f3690ad65fd353ce7fe1
Anushil007 <43786331+Anushil007@users.noreply.github.com>
2020-06-18 17:10:11 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- css -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- script -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <!-- navbar start -->
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#myNavbar">
+                <span class="glyphicon glyphicon-menu-hamburger"></span>
+            </button>
+           
+          
+         <div class="navbar-left logo"> 
+       <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 291 385.3" style="enable-background:new 0 0 291 385.3;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#000;}
+</style>
+<polygon class="st0" points="82.1,12 7.4,138.7 38.7,237.3 "/>
+<polygon class="st0" points="92.7,5.3 199.4,8.7 223.4,82.7 "/>
+<polygon class="st0" points="89.4,8.7 223.4,86.7 42.1,237.3 "/>
+<polygon class="st0" points="206.1,15.3 262.7,66 228.1,82.7 "/>
+<polygon class="st0" points="262.7,70 228.1,86 266.4,175.3 "/>
+<polygon class="st0" points="228.1,92.7 262.7,179.3 173.4,328 "/>
+<polygon class="st0" points="223.4,92.7 44.7,240 167.7,336 "/>
+<polygon class="st0" points="252.1,210.3 184.4,380.3 170.7,339.7 "/>
+<polygon class="st0" points="60.1,260.3 167.4,341 180.4,380.3 "/>
+</svg>
+          </div>
+		<h1 class="brand brand-name navbar-left"><div class"navbar-left">Info-Nity</h1>
+        </div>
+        <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="#intro">Home</a></li>
+   				<li><a href="blog.php">Blog</a></li>
+           <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              Category
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="signin.html">Sports</a>
+              <a class="dropdown-item" href="login.html">Entertainment</a>
+              <a class="dropdown-item" href="signin.html">Tech</a>
+              <a class="dropdown-item" href="signin.html">Career</a>
+            </div>
+          </li>
+          		<li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- navbar end -->
+<footer class = "footer text-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-12 p-3">
+            <h6 class="text-uppercase font-weight-bold ">Additional Information</h6>
+            <p>
+              Feel free to contact us for any type of information.
+            </p>
+          </div>
+          <div class="col-md-4 col-sm-12 p-3">
+            <h6 class="text-uppercase font-weight-bold">Contact</h6>
+              <br />inf0@infonity.com
+              <br />+977 4242121</p>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright text-center pb-3">&copy; 2019 Copyright:infonity.com</div>
+    </footer>
+
+
+</body>
+</html>
+
+
+
+    
