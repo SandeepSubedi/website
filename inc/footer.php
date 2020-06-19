9cdb405f9a0d87810536d044a731ad60abc12aa5
Sakar Paudel <43814730+sakar5157@users.noreply.github.com>
2020-06-19 21:28:34 +0545

    Base file

---
+++ inc/footer.php
@@ -0,0 +1,76 @@
+<!-- Footer -->
+	<footer class="mainfooter" role="contentinfo">
+  <div class="footer-middle">
+  
+      <div class="col-md-3 col-sm-6">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>Heading 1</h4>
+          <ul class="list-unstyled">
+            <li><a href="#"></a></li>
+            <li><a  href="#">Blog</a></li>
+            <li><a href="#">Sport</a></li>
+            <li><a href="#">Tech</a></li>
+            <li><a href="#">News and Updates</a></li>
+            <li><a href="#">Others</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <!--Column2-->
+        <div class="footer-pad">
+          <h4>Heading 2</h4>
+          <ul class="list-unstyled">
+            <li><a href="#">Hot News</a></li>
+            <li><a href="#">Recently added</a></li>
+            <li><a href="#">Trending</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <!--Column3-->
+        <div class="footer-pad">
+          <h4>Heading 3</h4>
+          <ul class="list-unstyled">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Disclamer</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li>
+              <a href="#"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <h4>Follow Us</h4>
+            <ul class="social-network social-circle">
+             <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+             <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+             <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+            </ul>       
+        </div>
+        <!-- Newsletter -->
+        <div class="col-md-3">
+            <div class="footer-widget">
+              <h3 class="footer-title">Join our Newsletter</h3>
+              <div class="footer-newsletter">
+                <form>
+                  <input class="input" type="email" name="newsletter" placeholder="Enter your email">
+                  <button class="newsletter-btn"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      <div class="footer-copyright">
+      <div style="text-align: center;">
+      <span>&copy; Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved.Info-Nity</span>
+    </div>
+  </div>
+  
+  </div>
+  </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php 
+	include $_SERVER['DOCUMENT_ROOT'].'config/init.php';
+	include 'inc/header.php';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- css -->
+    <link rel="stylesheet" type="text/css" href=".\assets\css\style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- script -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+</head>
+<body>
+
+
+
+
+<!-- container -->
+
+<div class="container">
+  <div class="row">
+    <div class="block col-md-10  bg-primary">
+      <p>useless </p>
+      <p> useless </p>
+      <p> useless</p>
+     
+      
+      <p>44 years. 114 players. 83 World Cup games including four finals. A tie. A first-ever Super Over in an ODI. A tie in the Super Over. It took all these for the creators of the game to finally lift the coveted World Cup trophy. The last time England were competitive in World Cups, ODIs did not have an identity of their own and existed as a condensed form of Test cricket. They failed to make it to the semifinals across six consecutive editions, a period which saw Sri Lanka, who never made it to a World Cup semifinal till then, lifting a World title and Kenya playing a semifinal.
+
+        After a turnaround post the infamous exit in Australia four years ago, England altered their outlook to ODI cricket, revamped their entire system and prioritized ODIs, even if it meant putting Test cricket on the backburner. The results are there to see and it culminated with their first major 50-over title win at the Home of Cricket. For England to win their first World title, they had to break almost all of their long-standing records in the tournament. </p>
+        44 years. 114 players. 83 World Cup games including four finals. A tie. A first-ever Super Over in an ODI. A tie in the Super Over. It took all these for the creators of the game to finally lift the coveted World Cup trophy. The last time England were competitive in World Cups, ODIs did not have an identity of their own and existed as a condensed form of Test cricket. They failed to make it to the semifinals across six consecutive editions, a period which saw Sri Lanka, who never made it to a World Cup semifinal till then, lifting a World title and Kenya playing a semifinal.
+
+        After a turnaround post the infamous exit in Australia four years ago, England altered th
+      </div>
+    
+    
+    <div class=" block row-md-2 col-md-1">
+      <p>useless </p>
+      <p> useless </p>
+      <p> useless</p>
+      <div class="email">    
+       <p>As the players used Sunday to take a day off from the rigmarole of the World Cup and prepare for Thursday's semi-final against Australia, the Ashes filled the airwaves with talk of Jason Roy's qualities as a Test opener.
+        sing fifty four times and converting one to a bumper 153.
+      </p>
+      </div>    
+    </div>
+    
+  </div>
+  </div>
+<?php include 'inc/footer.php'; ?>
+<!-- jQuery Plugins -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+	</body>
+</html>
